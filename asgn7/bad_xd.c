f63529df6cebd763f17d350d419d305c92a5e8f1
Yasmine <ysiala@ucsc.edu>
2024-12-04 17:03:48 -0800

    final?

--- asgn7/bad_xd.c
+++ asgn7/bad_xd.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h> 
+#include <ctype.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
@@ -9,35 +9,30 @@
 #define BYTES_PER_LINE 16
 
 int main(int argc, char *argv[]) {
-    //exit if more than one command line argument 
     if (argc > 2) {
         return 1;
     }
 
-    int input = STDIN_FILENO;  
+    int input = STDIN_FILENO;
     if (argc == 2) {
         input = open(argv[1], O_RDONLY);
         if (input == -1) {
+            perror("Failed to open file");
             return 1;
         }
-    }  
+    }
 
-    //initialize variables:
     unsigned char buffer[BYTES_PER_LINE];
     ssize_t bytes_read;
     size_t offset = 0;
-    
-    
     size_t buffer_index = 0;
-    
+
     while ((bytes_read = read(input, buffer + buffer_index, BYTES_PER_LINE - buffer_index)) > 0) {
         buffer_index += bytes_read;
 
-        //print when i have all 16 bytes 
         if (buffer_index == BYTES_PER_LINE) {
-            printf("%08zx: ", offset); // print the offset in hexadecimal
+            printf("%08x: ", (unsigned int)offset);  // Use %08x for printing int offsets
 
-            //hexadecimal column
             for (size_t i = 0; i < BYTES_PER_LINE; i++) {
                 printf("%02x", buffer[i]);
                 if ((i + 1) % 2 == 0) {
@@ -45,7 +40,6 @@ int main(int argc, char *argv[]) {
                 }
             }
 
-            //print ASCII column
             printf(" ");
             for (size_t i = 0; i < BYTES_PER_LINE; i++) {
                 if (isprint(buffer[i])) {
@@ -57,27 +51,24 @@ int main(int argc, char *argv[]) {
             printf("\n");
 
             offset += BYTES_PER_LINE;
-            buffer_index = 0; // Reset buffer for next line
+            buffer_index = 0;
         }
     }
 
-    //handle remaining bytes if they are less than 16
     if (buffer_index > 0) {
-        printf("%08zx: ", offset); // print the offset in hexadecimal
+        printf("%08x: ", (unsigned int)offset);
 
-        //print hexadecimal column
         for (size_t i = 0; i < BYTES_PER_LINE; i++) {
             if (i < buffer_index) {
                 printf("%02x", buffer[i]);
             } else {
-                printf("  "); // print spaces for missing bytes
+                printf("  ");
             }
             if ((i + 1) % 2 == 0) {
                 printf(" ");
             }
         }
 
-        //print ASCII column
         printf(" ");
         for (size_t i = 0; i < buffer_index; i++) {
             if (isprint(buffer[i])) {
@@ -89,7 +80,6 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
 
-    // clean up
     if (input != STDIN_FILENO) {
         close(input);
     }
